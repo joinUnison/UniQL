d068d9da787cff66544a0fc650f175868bbe23b7
Bryant Le <80173797+bnleft@users.noreply.github.com>
2024-03-28 17:54:44 -0500

    feat(graphql): add gqlgen wrapper boilerplate (#1)
    * feat(graphql): add NewServer
    
    * test(graphql): add NewServer
    
    * docs(README): add test coverage commands
    
    * chore(git): ignore build artifacts

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # UniQL
 
 Go GraphQL and Database aggregator 
+
+## Commands
+
+Run Tests
+```shell
+go test -v ./... -coverprofile="build/coverage.out"
+go tool cover -html="build/coverage.out" -o build/coverage.html
+```
---
+++ graphql/graphql_test.go
@@ -0,0 +1,44 @@
+package graphql
+
+import (
+	"context"
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/vektah/gqlparser/v2/ast"
+	"testing"
+)
+
+type MockExecutableSchema struct {
+	SchemaFunc     func() *ast.Schema
+	ComplexityFunc func(typeName, fieldName string, childComplexity int, args map[string]interface{}) (int, bool)
+	ExecFunc       func(ctx context.Context) graphql.ResponseHandler
+}
+
+func (m MockExecutableSchema) Schema() *ast.Schema {
+	return m.SchemaFunc()
+}
+
+func (m MockExecutableSchema) Complexity(typeName, fieldName string, childComplexity int, args map[string]interface{}) (int, bool) {
+	return m.ComplexityFunc(typeName, fieldName, childComplexity, args)
+}
+
+func (m MockExecutableSchema) Exec(ctx context.Context) graphql.ResponseHandler {
+	return m.ExecFunc(ctx)
+}
+
+func TestNewServer_Gqlgen_Success(t *testing.T) {
+	schema := &MockExecutableSchema{}
+
+	config := ServerConfig{
+		gqlgen: schema,
+	}
+
+	server := NewServer(config)
+
+	if server == nil {
+		t.Fatal("NewServer returned nil, expected a server instance")
+	}
+
+	if server.gqlgen == nil {
+		t.Fatal("server.gqlgen is nil, expected a handler.Server instance")
+	}
+}
